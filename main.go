c63aaa4ce592d74a24fd73c70355ab5bb7ea12cd
KimAdrian <adriankimutai5@gmail.com>
2022-01-03 20:19:36 +0300

    Cleanup

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"github.com/KimAdrian/RickandMorty_HttpClient/client"
+	"log"
+)
+
 func main() {
 
+	characters, err := client.FetchData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(characters)
 }
