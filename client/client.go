1130857ba13d9221ceabd9c9f067252f3a27fd98
KimAdrian <adriankimutai5@gmail.com>
2022-01-03 20:10:34 +0300

    Added client package

--- client/client.go
+++ client/client.go
@@ -1,1 +1,39 @@
 package client
+
+import (
+	"encoding/json"
+	"github.com/KimAdrian/RickandMorty_HttpClient/model"
+	"io"
+	"log"
+	"net/http"
+)
+
+func FetchData() (string, error) {
+
+	url := "https://rickandmortyapi.com/api/character"
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(res)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(res.Body)
+
+	var response model.ResponseStruct
+	//Decode data
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		log.Fatal("An error has occurred: ", err)
+	}
+
+	return response.TextOutput(), err
+}
